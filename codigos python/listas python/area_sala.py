3ab3b1d6df3a81358ee03ae0acd264223ac8a83a
JuuCarmona <168484665+JuuCarmona@users.noreply.github.com>
2024-11-08 19:24:39 -0300

    area_sala.py

---
+++ codigos python/listas python/area_sala.py
@@ -0,0 +1,4 @@
+largura=int(input("qual a largura da sala em metros quadrados? "))
+profundidade=int(input("qual a profundidade da sala em metros quadrados?"))
+area=largura*profundidade
+print("area:",area,"metros quadrados")
