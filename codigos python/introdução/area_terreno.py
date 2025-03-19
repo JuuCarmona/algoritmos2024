235e93c0d0574f9fa4f66c12ecbfe6f6c902503a
JuuCarmona <168484665+JuuCarmona@users.noreply.github.com>
2025-03-18 23:14:17 -0300

    area_terreno.py

--- codigos python/introdução/area_terreno.py
+++ codigos python/introdução/area_terreno.py
@@ -1,5 +1,6 @@
 largura=int(input("qual a largura em metros?"))
 profundidade=int(input("qual a profundidade em metros?"))
+
 area=largura*profundidade
 hectar=area/10000
 print("area:",hectar,"hectares")
