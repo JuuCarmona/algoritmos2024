28bce4beb33a72fb02b49cbd19ff78cc8499844a
JuuCarmona <168484665+JuuCarmona@users.noreply.github.com>
2025-03-18 22:53:05 -0300

    conta.py

---
+++ codigos python/introdução/conta.py
@@ -0,0 +1,5 @@
+rs=int(input("qual o valor da refeição com suco?"))
+pp=int(input("qual o valor do prato principal?"))
+s=int(input("qual o valor da sobremesa?"))
+total=(rs)+(pp)+(s)
+print("o valor total da compra é R${:2f}".format(total))
