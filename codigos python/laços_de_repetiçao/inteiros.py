aabd190537fc127eb95a614959bdfbd3c523e7ee
JuuCarmona <168484665+JuuCarmona@users.noreply.github.com>
2025-03-19 16:38:53 -0300

    inteiros.py

---
+++ codigos python/laços_de_repetiçao/inteiros.py
@@ -0,0 +1,13 @@
+#imprime numeros inteiros de 1 a 20 
+numero = 1
+
+while numero <= 20:
+    print(numero)
+    numero = numero + 1
+
+#imprime uma sequencia de inteiros ate no minimo 0
+p = 5
+numero = 100
+while numero >= 0:
+    print(numero)
+    numero = numero - 5
