d9b6f0ba871b37c72491a706c8dd5c76ceb66683
JuuCarmona <168484665+JuuCarmona@users.noreply.github.com>
2025-03-18 23:16:42 -0300

    retorno_reciclaveis.py

--- codigos python/introdução/retorno_reciclaveis.py
+++ codigos python/introdução/retorno_reciclaveis.py
@@ -1,4 +1,5 @@
 n1=int(input("qual a quantidade de vasilhames de um liteo ou menos?"))
 n2=int(input("qual a quantidade de vasilhames de um litro ou mais?"))
+
 total=(n1*0.10)+(n2*0.25)
 print("o valor total de créditos é R${:.2f}".format(total))
