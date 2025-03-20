9c1ecefda96162cdfa755723ce7502bb9c191bb9
JuuCarmona <168484665+JuuCarmona@users.noreply.github.com>
2025-03-20 11:16:15 -0300

    decimal_para_binario.py

--- codigos python/recursividade/decimal_para_binario.py
+++ codigos python/recursividade/decimal_para_binario.py
@@ -1,15 +1,21 @@
-def bininterativo(q):
-    if q == 0:
-        return "0"
+def binario_recursivo(n):
+    if n < 0:
+        return "Erro: O número deve ser não negativo."
+    elif n == 0:
+        return '0'
+    elif n == 1:
+        return '1'
     else:
-        resultado = ""
-        while q > 0:
-            resto = q % 2
-            resultado = str(resto) + resultado
-            q = q // 2
-        return resultado
-    
-numero = 8
-resultado_final = bininterativo(numero)
+        proximo_digito = str(n % 2)
+        resto = binario_recursivo(n // 2)
+        return resto + proximo_digito
 
-print(f"A representação binária é:{bininterativo(numero)}")
+def main():
+    numero = int(input("Digite um número decimal não negativo: "))
+    if numero < 0:
+        print("Erro: O número deve ser não negativo.")
+    else:
+        resultado_binario = binario_recursivo(numero)
+        print(f"A representação é: {resultado_binario}")
+
+main()
